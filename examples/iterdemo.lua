e093c21dc56d3ff2d5be93d2d587bd006ab52e61
Hans-Christian Esperer <hc@hcesperer.org>
2026-07-21 12:58:54 +0200

    Extend the iterator example by a "runaway loop yielder"

--- examples/iterdemo.lua
+++ examples/iterdemo.lua
@@ -7,11 +7,35 @@ function my_iter()
 	end)
 end
 
+function runaway_loop_iterator()
+	return coroutine.wrap(function()
+		local callme = function(next_fun, c)
+			print("    runaway loop returning", c)
+                        coroutine.yield(c)
+			next_fun(next_fun, c + 1)
+		end
+		callme(callme, 0)
+	end)
+end
+
+
 local itermll = require "itermll"
 print("======================================================================")
 print("STRICT RUN")
 itermll.runStrict()
 
+print()
 print("======================================================================")
 print("NONSTRICT RUN")
 itermll.run()
+
+print()
+print("======================================================================")
+print("TAKE n ITEMS ONLY:")
+print("Take 10 items only:")
+itermll.runPartly(10)
+print("Take 20 items only:")
+itermll.runPartly(20)
+print("Take 30 items only:")
+itermll.runPartly(30)
+
--- examples/itermll.mll
+++ examples/itermll.mll
@@ -8,9 +8,9 @@
 -- statements should interleave. If the list is strictly
 -- evaluated, the statements would not interleave.
 
-myIter :: LuaIterator "my_iter" [Integer]
-
-myIter' :: LuaIterator "my_iter" [Integer]
+myIter              :: LuaIterator "my_iter" [Integer]
+myIter'             :: LuaIterator "my_iter" [Integer]
+runawayLoopIterator :: LuaIterator "runaway_loop_iterator" [Integer]
 
 export run :: IO ()
 run = flip mapM_ myIter $ \item ->
@@ -20,6 +20,9 @@ export runStrict :: IO ()
 runStrict = let myIter'' = length myIter' `seq` myIter' in
         flip mapM_ myIter'' $ \item ->
             putStrLn $ "Streaming from Lua to mata-ll, we got: " <> show item
+
+export runPartly :: Integer -> IO ()
+runPartly c = print $ take c runawayLoopIterator
 
 main :: IO ()
 main = putStrLn "You need to compile this example to Lua, than run iterdemo.lua"
